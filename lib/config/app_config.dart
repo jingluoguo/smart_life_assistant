fadb8fe722ffd8a0ceb1a8fee442dc1ad987353d
jingluoguo <jingluoguo@163.com>
2025-10-15 11:53:48 +0800

    修改readme.md

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -3,16 +3,19 @@ class AppConfig {
   AppConfig._();
 
   // ==================== 和风天气配置 ====================
-  static const String qWeatherApiHost = '';
-  static const String qWeatherProjectId = '';
-  static const String qWeatherCredentialId = '';
+  static const String qWeatherApiHost = 'mu52q4jwqd.re.qweatherapi.com';
+  static const String qWeatherProjectId = '4DKTM9MVMQ';
+  static const String qWeatherCredentialId = 'T6WKR4U3MQ';
   static const String qWeatherPrivateKey = '''
+-----BEGIN PRIVATE KEY-----
+MC4CAQAwBQYDK2VwBCIEINiaaMH4nmvI2Xe7tNUz8814vofZ2c/nq8oRstRFmF8k
+-----END PRIVATE KEY-----
 ''';
 
   // ==================== 通义千问配置 ====================
   static const String dashScopeApiUrl =
       'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions';
-  static const String dashScopeApiKey = '';
+  static const String dashScopeApiKey = 'sk-9a65c355846e407292f7fcd77220b6d7';
   static const String dashScopeModel = 'qwen3-max';
 
   // ==================== 网络配置 ====================
